3ab15964510892800879060d443f5e52bc121b23
Hilful <hilful25@gmail.com>
2022-02-03 01:37:37 +0600

    2nd commit

---
+++ loveCalculator.js
@@ -0,0 +1,6 @@
+var x = prompt("What is your name ?");
+var y = prompt("What is your lovers name ?");
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) +1 ;
+alert(x +" and your lover " + y + " are in " + n + " percent love");
